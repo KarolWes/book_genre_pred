f0dfd84aaa784e98354f71f53a9d99f677750d48
KarolWes <karol.wesolowski01@gmail.com>
2022-11-05 19:13:36 +0100

    Initial, preprocessing

---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
+
+#additional downloads
+#nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('wordnet')
+#nltk.download('omw-1.4')
+
+
+def nlp_cleaning(data):
+    english_stops = set(stopwords.words("english"))
+    wl = WordNetLemmatizer()
+    new_col = []
+    for summary in data["summary"]:
+        print(".", end="")
+        tokens = word_tokenize(summary.lower()) #split into tokens
+        letters_only = [token for token in tokens if token.isalpha()] #remove non-alpha
+        no_stops = [token for token in letters_only if token not in english_stops] #remove stop words
+        stems = [wl.lemmatize(token) for token in no_stops] #remove plural forms
+        stems = [wl.lemmatize(token, 'v') for token in stems] #change verbs to base form
+        word_dict = Counter(stems)
+        new_col.append(word_dict)
+    data["dict"] = new_col
+
+
+if __name__ == '__main__':
+    data = pd.read_csv("data.csv", index_col=0)
+    print(data.head())
+    print("shape of the data frame ", data.shape)
+    print("does it contains missing values?\n", data.isna().any(), "\n_______")
+    nlp_cleaning(data)
+    print(data.head())
+
