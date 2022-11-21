64801b6395a821983cb82eef3a5aef71408dd9be
KarolWes <karol.wesolowski01@gmail.com>
2022-11-21 12:44:13 +0100

    machine learning function + cleaning

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,7 +10,11 @@ from nltk.stem import WordNetLemmatizer
 from collections import Counter
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
 
 
 # additional downloads
@@ -35,6 +41,7 @@ def nlp_cleaning(data):
         new_col_stems.append(stems)
     data["clean"] = new_col_stems
     data["word_dictionary"] = new_col_dicts
+    data.to_csv('out.csv')
 
 
 def string_to_list(s):
@@ -57,22 +64,52 @@ def sum_counter(data):
     return genre_dict_counter
 
 
+def graph(data):
+    plt.figure(figsize=(10, 10))
+    g = sns.countplot(x="genre", data=data)
+    plt.xticks(rotation=45)
+    fig = g.get_figure()
+    fig.savefig("wykres1.png")
+    plt.show()
+
+def words_dict_by_genre(data):
+    dict = sum_counter(data)
+    for key in dict.keys():
+        print(key + ": " + str(dict[key].most_common(10)))
+
+
+def fit(X_train, X_test, y_train, y_test):
+    ans = []
+    models = [KNeighborsClassifier(), LogisticRegression()]
+    for model in models:
+        # model = OneVsRestClassifier(model)
+        start = time.process_time()
+        model.fit(X_train, y_train)
+        pred = model.predict(X_test)
+        fin = time.process_time()
+        ac = accuracy_score(pred, y_test)
+        f1 = f1_score(pred, y_test, average="macro")
+        ans.append([type(model).__name__, ac, f1, (fin-start)*1000])
+    df = pd.DataFrame(ans, columns=["model", "accuracy", "F1 score", "time ms"])
+    return df
+
+
 if __name__ == '__main__':
     data = pd.read_csv("out.csv", index_col=0)
     print(data.head())
     print("shape of the data frame ", data.shape)
     print("does it contains missing values?\n", data.isna().any(), "\n_______")
-    # plt.figure(figsize=(10, 10))
-    # g = sns.countplot(x="genre", data=data)
-    # plt.xticks(rotation=45)
-    # fig = g.get_figure()
-    # fig.savefig("wykres1.png")
-    # plt.show()
+    # graph(data)
     # nlp_cleaning(data)
-    # data.to_csv('out.csv')
-    dict = sum_counter(data)
-    for key in dict.keys():
-        print(key + ": " + str(dict[key].most_common(10)))
+    # words_dict_by_genre(data)
     # print(data.head())
-    # X_data = CountVectorizer.fit_transform(data["clean"])
-    # X_train, X_test, y_train, y_test = train_test_split(X_data, data["genre"], test_size=0.25, random_state=79)
+    data["genre"] = LabelEncoder().fit_transform(data["genre"])
+    X_data = CountVectorizer().fit_transform(data["clean"])
+    X_train, X_test, y_train, y_test = train_test_split(X_data, data["genre"], test_size=0.25, random_state=79)
+    print(y_test)
+    genre_distribution = Counter(y_test)
+    print(genre_distribution)
+
+    df = fit(X_train, X_test, y_train, y_test)
+    print(df)
+
