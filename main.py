fee6967f69924ce5d57afceba01fe2bdd8f07745
KarolWes <karol.wesolowski01@gmail.com>
2023-01-18 17:28:10 +0100

    Presentation in jupyter part 4 - one vs one

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
@@ -82,12 +82,24 @@ def words_dict_by_genre(data):
         print(key + ": " + str(dict[key].most_common(10)))
 
 
-def fit(X_train, X_test, y_train, y_test, old_labels):
+def fit_best(X_train, X_test, y_train, y_test, c):
+    model = OneVsRestClassifier(LogisticRegression(C=c))
+    model.fit(X_train, y_train)
+    pred = model.predict(X_test)
+    ac = accuracy_score(pred, y_test)
+    f1 = f1_score(pred, y_test, average="macro")
+    ans = [c, ac, f1]
+    return ans
+
+
+def fit(X_train, X_test, y_train, y_test, old_labels, classifier):
     ans = []
     models = [KNeighborsClassifier(), LogisticRegression(), MultinomialNB(), SVC()]
-    graph, axes = plt.subplots(len(models)//2, 2, figsize=(18,10))
+    graph, axes = plt.subplots(len(models) // 2, 2, figsize=(18, 12))
+    graph.suptitle(str(classifier).split('.')[-1][:-2], size=24)
+    graph.tight_layout(pad=5.0)
     for i, model in enumerate(models):
-        model = OneVsRestClassifier(model)
+        model = classifier(model)
         start = time.process_time()
         model.fit(X_train, y_train)
         pred = model.predict(X_test)
@@ -110,54 +122,25 @@ def fit(X_train, X_test, y_train, y_test, old_labels):
         fig.yaxis.set_ticklabels(old_labels)
         fig = fig.get_figure()
         fig.savefig(str(model) + ".png")
-        fig = sns.heatmap(cfm / cfm.sum(axis=1)[:, None] * 100, ax=axes[i//2, i%2], annot=True, cmap='Greens', vmax=100)
+        fig = sns.heatmap(cfm / cfm.sum(axis=1)[:, None] * 100, ax=axes[i // 2, i % 2], annot=True, cmap='Greens',
+                          vmax=100)
         fig.tick_params(labelrotation=45)
         fig.xaxis.set_ticklabels(old_labels)
         fig.yaxis.set_ticklabels(old_labels)
+        title = str(model).split('=')[1][:-3]
+        fig.title.set_text(title)
 
     df = pd.DataFrame(ans, columns=["model", "accuracy", "F1 score", "time ms"])
-    graph.savefig("grid.png")
-    return df
-
-
-def fit_best(X_train, X_test, y_train, y_test, old_labels):
-    ans = []
-    for c in range(1,11):
-        model = OneVsRestClassifier(LogisticRegression(C=c/10))
-        start = time.process_time()
-        model.fit(X_train, y_train)
-        pred = model.predict(X_test)
-        fin = time.process_time()
-        ac = accuracy_score(pred, y_test)
-        f1 = f1_score(pred, y_test, average="macro")
-
-        ans.append([c, ac, f1, (fin - start) * 1000])
-
-        cfm = confusion_matrix(y_test, pred)
-
-        plt.figure(figsize=(10, 10))
-        fig = sns.heatmap(cfm / cfm.sum(axis=1)[:, None] * 100, annot=True, cmap='Greens', vmax=100)
-        fig.set_title("Logistic Regression (c = " + str(c) + ") [%]")
-        fig.set_xlabel("Predicted")
-        fig.set_ylabel("Real")
-        plt.xticks(rotation=45)
-        plt.yticks(rotation=45)
-        fig.xaxis.set_ticklabels(old_labels)
-        fig.yaxis.set_ticklabels(old_labels)
-        fig = fig.get_figure()
-        fig.savefig(str(model) + ".png")
-        plt.show()
-
-    df = pd.DataFrame(ans, columns=["c", "accuracy", "F1 score", "time ms"])
+    graph.savefig("grid_ovo.png")
     return df
 
 
 if __name__ == '__main__':
-    data = pd.read_csv("data.csv", index_col=0)
-    print(data.head())
-    print("shape of the data frame ", data.shape)
-    print("does it contains missing values?\n", data.isna().any(), "\n_______")
-    graph(data)
+    # data = pd.read_csv("data.csv", index_col=0)
+    # print(data.head())
+    # print("shape of the data frame ", data.shape)
+    # print("does it contains missing values?\n", data.isna().any(), "\n_______")
+    # graph(data)
     # nlp_cleaning(data, "out.csv")
     data = pd.read_csv("out.csv")
     # words_dict_by_genre(data)
@@ -166,10 +149,25 @@ if __name__ == '__main__':
     data["genre"] = encoder.fit_transform(data["genre"])
     old_labels = dict(zip(encoder.classes_, range(len(encoder.classes_))))
     X_data = CountVectorizer().fit_transform(data["clean"])
+    # X_train, X_test, y_train, y_test = train_test_split(X_data, data["genre"], test_size=0.25, random_state=79)
+    # dist = Counter(y_test)
+    # genre_distribution = np.array([dist[i] for i in range(len(dist))])
+    # print(dist)
+    # res = []
+    # start = time.process_time()
+    # for r in range(3):
+    #     X_train, X_test, y_train, y_test = train_test_split(X_data, data["genre"], test_size=0.25, random_state=r*17+13)
+    #     for c in range(1,11):
+    #         print(r, " ", c)
+    #         res.append(fit_best(X_train, X_test, y_train, y_test, c/10))
+    # df = pd.DataFrame(res, columns=["reg. param.", "accuracy", "F1 score"])
+    # # df = df.groupby('reg. param.').mean()
+    # fin = time.process_time()
+    # print((fin - start))
     X_train, X_test, y_train, y_test = train_test_split(X_data, data["genre"], test_size=0.25, random_state=79)
-    dist = Counter(y_test)
-    genre_distribution = np.array([dist[i] for i in range(len(dist))])
-    print(dist)
+    print("___")
+    print(fit(X_train, X_test, y_train, y_test,old_labels, OneVsOneClassifier))
+    print("___")
 
-    df = fit(X_train, X_test, y_train, y_test, old_labels)
-    print(df)
+    #df = fit(X_train, X_test, y_train, y_test, old_labels)
+    # print(df)
